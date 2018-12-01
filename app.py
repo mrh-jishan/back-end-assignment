c1490f07ea1f59759035fbc3b3e7caedf1073d27
Robin Hassan <ra.onerobin@gmail.com>
2018-12-02 04:02:35 +0800

    FIX Mapping for room

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 class Room:
     def __init__(self, adults, children, infants):
-        if adults > 3: raise ValueError('Max 3 adults allowed')
-        if children > 3: raise ValueError('Max 3 children allowed')
-        if infants > 3: raise ValueError('Max 3 infants allowed')
-        if adults < 1: raise Exception('Minimum 1 adult is required, No room will have only children or infants')
+        # if adults > 3: raise ValueError('Max 3 adults allowed')
+        # if children > 3: raise ValueError('Max 3 children allowed')
+        # if infants > 3: raise ValueError('Max 3 infants allowed')
+        # if adults < 1: raise Exception('Minimum 1 adult is required, No room will have only children or infants')
         self.adults = adults
         self.children = children
         self.infants = infants
@@ -25,33 +25,45 @@ class Booking:
         if not d: raise ValueError('Minimum 1 adult is required')
         self._adults = d
 
+    def get_room_mapping(self, optimized_min_room):
+        room_list = []
+        a = optimized_min_room
+        while a:
+            ad = int(self.adults / optimized_min_room) + 1 if (not (self.adults % optimized_min_room) == 0) and len(
+                room_list) == 0 else int(
+                self.adults / optimized_min_room)
+            ch = int(self.children / optimized_min_room) + 1 if (not (self.children % optimized_min_room) == 0) and len(
+                room_list) == 0 else int(
+                self.children / optimized_min_room)
+            inf = int(self.infants / optimized_min_room) + 1 if (not (self.infants % optimized_min_room) == 0) and len(
+                room_list) == 0 else int(
+                self.infants / optimized_min_room)
+
+            room_list.append(Room(ad, ch, inf).__dict__)
+            a -= 1
+
+        return room_list
+
     def assign_room(self):
+        # Calculate max required room for each category
         max_room_children = int(self.children / 3) if (self.children % 3) == 0 else int(self.children / 3) + 1
         max_room_infants = int(self.infants / 3) if (self.infants % 3) == 0 else int(self.infants / 3) + 1
+        max_room_adult = int(self.adults / 3) if (self.adults % 3) == 0 else int(self.adults / 3) + 1
+
+        # # Check if per booking has more than 3 room
         max_room = max_room_children if max_room_children >= max_room_infants else max_room_infants
-        if max_room>3 : raise Exception('Per booking maximum number of rooms will be only 3')
+        if max_room > 3: raise Exception('Per booking maximum number of rooms will be only 3')
 
-        print('max room---------------------------', max_room)
-
-
-
-        # max_room_adult = int(self.adults / 3) if (self.adults % 3) == 0 else int(self.adults / 3) + 1
-
-
-        # if (self.adults < max_room): raise Exception('No room will have only children or infants')
-        # max_room = max_room if max_room >= max_room_adult else max_room_adult
-
-        # room_list = []
-        # booking1 = {1: Room(3, 3, 3)}
-        # booking2 = {2: Room(3, 3, 3)}
-        #
-        # room_list.append(booking1)
-        # room_list.append(booking2)
-        # print(booking1.__dict__)
-        # total_room_adult = (self._adults % 3) if (self._adults % 3) == 0 else (self._adults % 3) + 1
-        # return dict({'total_room': max_room})
+        # # Check if any room has only infants or children
+        if (self.adults < max_room): raise Exception('No room will have only children or infants')
+        # Get final total number of most optimized room
+        optimized_min_room = max_room if max_room >= max_room_adult else max_room_adult
+        return optimized_min_room
 
 
-booking = Booking(7, 0, 14)
-room_details = booking.assign_room()
+booking = Booking(3, 4, 7)
+optimized_min_room = booking.assign_room()
+room_mapping = booking.get_room_mapping(optimized_min_room)
+print('The optimized number of room(s): ', optimized_min_room)
+print('The optimized room(s) mapping: ', room_mapping)
 # print(room_details)
